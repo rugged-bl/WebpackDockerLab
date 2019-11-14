8a02972423354041b4c8e36b3fe3d39311995581
Rugged-bl <vasia.shumilov@gmail.com>
2019-11-14 20:59:05 +0300

    webpack project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import './style.css';
+
+let textarea = document.querySelector('.textarea');
+let wordsValue = document.querySelector('.words-value');
+let charactersValue = document.querySelector('.characters-value');
+let sentencesValue = document.querySelector('.sentences-value');
+
+let charactersCount = function (str) {
+  return str.length;
+};
+
+let wordsCount = function (str) {
+  return str.trim().split(/\s+/).length;
+};
+
+let sentencesCount = function (str) {
+  return str.split('.').length - 1;
+};
+
+let onInputChange = function () {
+  charactersValue.textContent = 'Количество символов в тексте: ' + charactersCount(textarea.value);
+  wordsValue.textContent = 'Количество слов в тексте: ' + wordsCount(textarea.value);
+  sentencesValue.textContent = 'Количество преложений в тексте: ' + sentencesCount(textarea.value);
+};
+
+textarea.addEventListener('input', onInputChange);
